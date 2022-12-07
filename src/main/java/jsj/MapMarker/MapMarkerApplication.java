f910bb920955f5c46f0ad1b7a373c15444b6b678
[josun] maker <seongjin3378@naver.com>
2022-12-07 19:40:07 +0900

    환경세팅 완료

---
+++ src/main/java/jsj/MapMarker/MapMarkerApplication.java
@@ -0,0 +1,13 @@
+package jsj.MapMarker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapMarkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapMarkerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jsj/MapMarker/MapMarkerApplicationTests.java
@@ -0,0 +1,13 @@
+package jsj.MapMarker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapMarkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
